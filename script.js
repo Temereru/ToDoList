0bea8d6ad3e9d2d23c73ddbf00c0dc34eba61a44
Tomer David <younrg@gmail.com>
2016-03-16 08:54:59 +0200

    fixed being able to post an empty mission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	    $('#button').click(function(){
         var toAdd = $('input[name=toDoText]').val();
+        if(toAdd != ""){
         $('.List').append('<div class="listItem"><input type="checkbox">' + toAdd + '   <div class="remove">remove</div></div>');
         $('input[name=toDoText]').val("");
+      }
     });
 	    $(document).on('click','.remove',function(){$(this).parent().remove()});
 });
